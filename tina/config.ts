f3d60d5c70edb9a6f09ad32dab3b539d01b188bf
Jastk <jakubvagner409@gmail.com>
2026-03-04 18:09:45 +0100

    Update

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tinacms";
 
 const branch =
-  process.env.GITHUB_BRANCH ||
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
   "main";
