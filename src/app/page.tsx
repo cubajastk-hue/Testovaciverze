816919967f18feb2e214f368121cf284f98d3b4d
cubajastk-hue <cubajastk@gmail.com>
2026-05-27 20:39:35 +0000

    fix: vyčištění page.tsx a aktualizace lockfile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,10 @@ export default async function Home() {
   const res = await client.queries.page({ relativePath: "home.mdx" });
 
   return (
-    <main className="min-h-screen bg-white">
-      {/* Container pro vycentrování obsahu a horní odsazení */}
-      <div className="max-w-2xl mx-auto px-6 py-20">
-        <PageComponents
-          data={JSON.parse(JSON.stringify(res.data))}
-          query={res.query}
-          variables={res.variables}
-        />
-      </div>
-    </main>
+    <PageComponents
+      data={JSON.parse(JSON.stringify(res.data))}
+      query={res.query}
+      variables={res.variables}
+    />
   );
 }
